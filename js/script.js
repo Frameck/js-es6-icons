e1dfe7f6a977ab69d92909e0ac0650a028a94217
Frameck <fra9879@gmail.com>
2021-10-26 19:36:10 +0200

    Completed exercise

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// provided input
 const iconsArray = [
 	{
 		name: 'cat',
@@ -96,3 +97,101 @@ const iconsArray = [
 		family: 'fas'
 	}
 ];
+
+
+// DOM elements
+const container = document.querySelector('.container')
+const select = document.getElementById('categories-select')
+
+// STRUTTURA: {animal: Array(8), vegetable: Array(4), user: Array(4)}
+const newIcons = sortIntoCategories(iconsArray)
+
+// genero tutte le card al caricamento della pagina
+generateCards('all', newIcons)
+
+// inserisco i valori nella select in base alle categorie
+populateSelect(newIcons)
+
+
+
+
+function populateSelect(obj) {
+    const types = Object.keys(obj)
+    let options = '<option value="all">All</option>'
+
+    types.forEach(type => {
+        options += `<option value="${type}">${type[0].toUpperCase() + type.slice(1)}</option>`
+    })
+
+    select.innerHTML = options
+}
+
+
+function sortIntoCategories(array) {
+    let newIcons = {}
+    array.forEach(icon => {
+        const { name, prefix, type, family } = icon
+        if (!newIcons[type]) {
+            newIcons[type] = []
+        }
+        newIcons[type].push({
+            name,
+            prefix,
+            type,
+            family
+        })
+    })
+    return newIcons
+}
+
+
+function generateCards(category, obj) {
+    let cardsToHtml = ''
+    
+    if (!obj[category]) {
+        for (const key in obj) {
+            if (Object.hasOwnProperty.call(obj, key)) {
+                const iconsArray = obj[key]
+
+                iconsArray.forEach(icon => {
+                    const { name, prefix, type, family } = icon
+                    cardsToHtml += createCardElement(name, prefix, type, family)
+                })
+            }
+        }
+    } else {
+        obj[category].forEach(icon => {
+            const { name, prefix, type, family } = icon
+            cardsToHtml += createCardElement(name, prefix, type, family)
+        })
+    }
+    
+    container.innerHTML = cardsToHtml
+}
+
+
+function createCardElement(name, prefix, type, family) {
+    let color = ''
+    // determine the color
+    if (type === 'animal') {
+        color = 'blue'
+    } else if (type === 'vegetable') {
+        color = 'orange'
+    } else {
+        color = 'yellow'
+    }
+
+    const card = 
+    `<div class="card">
+        <i class="${family} ${prefix + name}" style="color: ${color}"></i>
+        <div>${name.toUpperCase()}</div>
+    </div>`
+
+    return card
+}
+
+
+select.addEventListener('change', function () {
+    const type = select.value
+    generateCards(type, newIcons)
+})
